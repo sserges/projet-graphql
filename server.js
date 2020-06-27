1a32b688eb67ff4a8b5e0b5c4c38cfba859fccad
sserges <agounkeserge@gmail.com>
2020-06-27 12:54:11 +0000

    Server setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const expressGraphQL = require("express-graphql");
+
+const server = express();
+server.use(
+  "/salutGraphQL",
+  expressGraphQL({
+    graphiql: true,
+  }),
+);
+
+server.listen(4000, () => {
+  console.log("Serveur est en écoute sur le port 4000");
+});
